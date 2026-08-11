457e6701ee16b0512c6513725590bbadc2ff2fe3
fabriciovalentinlopez6-ux <fabriciovalentinlopez6@gmail.com>
2026-08-11 14:01:14 -0300

    Add files via upload

---
+++ sisf/proyecto JS basico/script.js
@@ -0,0 +1,15 @@
+document.getElementById('dogo').onclick = function() {
+  window.location.href = 'dogo.html';
+};
+
+document.getElementById('husky').onclick = function() {
+  window.location.href = 'husky.html';
+};
+
+document.getElementById('pastor').onclick = function() {
+  window.location.href = 'pastor.html';
+};
+
+document.getElementById('chop').onclick = function() {
+  window.location.href = 'chop.html';
+};
